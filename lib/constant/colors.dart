7f4972c249a1e6fada61b326bcddcd7985dba1d2
oguzhancotur <oguzhancotur55@gmail.com>
2024-03-15 21:39:34 +0300

    Calculator App

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF212224);
+  static const secondaryColor = Color(0xFFff4f00);
+  static const secondary2Color = Color(0xFF26282D);
+}
